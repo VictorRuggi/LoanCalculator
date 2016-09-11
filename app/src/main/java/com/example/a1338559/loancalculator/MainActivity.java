4feacd1a5c55fa4c6d9a633c732f208fb1f707a4
VictorRuggi <victorruggiitalian@hotmail.com>
2016-09-10 23:04:21 -0400

    Add files via upload

--- app/src/main/java/com/example/a1338559/loancalculator/MainActivity.java
+++ app/src/main/java/com/example/a1338559/loancalculator/MainActivity.java
@@ -20,18 +20,20 @@ public class MainActivity extends AppCompatActivity {
     //Event handler
     protected void onCalculate(View view)
     {
-        int LOAN_AMOUNT = R.id.loan;
-        int YEARS = R.id.years;
-        double INTEREST_RATE = (double)R.id.interest;
-
+        //References to view objects
         etloan = (EditText) findViewById(R.id.loan);
         etyears = (EditText) findViewById(R.id.years);
         etint = (EditText) findViewById(R.id.interest);
 
-        //Get Data Input
+        //Get Data Input, store as a string
         String strloan = etloan.getText().toString();
         String stryears = etyears.getText().toString();
         String strint = etint.getText().toString();
+
+        //Convert strings to numbers
+        int LOAN_AMOUNT = Integer.parseInt(strloan);
+        int YEARS = Integer.parseInt(stryears);
+        double INTEREST_RATE = Double.parseDouble(strint);
 
         double monthlyPayment;
         double monthlyInterestRate;
@@ -40,14 +42,11 @@ public class MainActivity extends AppCompatActivity {
         if (YEARS != 0 && INTEREST_RATE != 0)
         {
             //calculate the monthly payment
-            monthlyInterestRate = INTEREST_RATE / 1200;
+            monthlyInterestRate = INTEREST_RATE / (YEARS*12);
             numberOfPayments = YEARS * 12;
 
-            monthlyPayment =
-                    (LOAN_AMOUNT * monthlyInterestRate) /
-                            (1 - (1 / Math.pow ((1 + monthlyInterestRate), numberOfPayments)));
+            monthlyPayment = (LOAN_AMOUNT * monthlyInterestRate) / (1 - (1 / Math.pow (1 + monthlyInterestRate, numberOfPayments)));
 
-            monthlyPayment = Math.round (monthlyPayment * 100) / 100.0;
             msg = (TextView) findViewById(R.id.resultBox);
             msg.setVisibility(TextView.VISIBLE);
             msg.setText("Loan_amount: $" + strloan + "\nYears: " + stryears + "\nInterest rate: %" + strint
